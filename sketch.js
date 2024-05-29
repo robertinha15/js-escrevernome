4346b91861903f965931a2b5f64ac75053f3d27d
robertinha15 <00001092176482sp@al.educacao.sp.gov.br>
2024-05-29 15:22:09 -0300

    adicionado arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("white");
+    
+    stroke("blue")
+    fill("red")
+    rect(0, 0, 100, 150)
+    
+    
+  }
